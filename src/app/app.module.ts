b69f01a7cc82314f3c7d7cfe8f2347f6b0d72a00
steve.hamilton.64@gmail.com <steve.hamilton.64@gmail.com>
2021-05-17 17:19:43 -0500

    Success add of state counties census list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { StateCountiesCensusListComponent } from './components/state/state-counties-census-list/state-counties-census-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    StateCountiesCensusListComponent
   ],
   imports: [
     BrowserModule,
