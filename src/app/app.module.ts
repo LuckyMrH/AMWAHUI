fdd7599e44b91f3d557ca3aa184dc6ba56b77e4f
Steve Hamilton <steve.hamilton.64@gmail.com>
2021-06-04 22:50:17 -0500

    Revert to 3e4dafd15143494593710ef79e7a8b2f23b2a18b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { StateCountiesCensusListComponent } from './components/state/state-counties-census-list/state-counties-census-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
-    StateCountiesCensusListComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
